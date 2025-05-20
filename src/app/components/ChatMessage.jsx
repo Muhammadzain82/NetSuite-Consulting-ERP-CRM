8f84cb6717a7b9f65d5379207c55c2bb181e1103
Muhammad Zain <muhammadzainrajput82@gmail.com>
2025-05-20 15:42:44 +0500

    code updated

--- src/app/components/ChatMessage.jsx
+++ src/app/components/ChatMessage.jsx
@@ -33,7 +33,7 @@ import React from "react";
  const ChatMessage = ({ isUser, message, label }) => {
   return (
     <div className={`text-lg flex items-center ${isUser ? 'justify-end' : ''} font-normal gap-4`}>
-      {!isUser && <img src="/images/client.png" loading="lazy" alt="Client"/>}
+      {!isUser && <img src="/images/chat.png" loading="lazy" alt="Client"/>}
       <p className={`whitespace-pre-wrap shadow-md ${
         isUser 
           ? 'text-white bg-[#0B56E0]' 
@@ -41,7 +41,7 @@ import React from "react";
       } shadow-[#7b8a9c3a] px-6 py-4 rounded-xl h-fit`}>
         {isUser ? `${label?.replace("...", "") || ""} ${message}` : message}
       </p>
-      {isUser && <img src="/images/client.png" loading="lazy" alt="Client" />}
+      {isUser && <img src="/images/chat.png" loading="lazy" alt="Client" />}
     </div>
   );
 };
